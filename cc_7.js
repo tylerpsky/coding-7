bdee30c7737383c977d7445a9311bed7160e2472
Tyler Paiewonsky <tylerpsky@gmail.com>
2025-02-16 13:21:21 -0500

    Task 5 - Business Loan Interest Calculation.

--- cc_7.js
+++ cc_7.js
@@ -61,3 +61,13 @@ function calculateShippingCost(weight, location, expedited = false) {
 
 console.log(calculateShippingCost(10, "USA", true)); 
 console.log(calculateShippingCost(5, "Canada", false));
+
+//Task 5
+
+function calculateLoanInterest(principal, rate, years) {
+    let interest = principal * rate * years;
+    return `Total Interest: $${interest.toFixed(2)}`;
+}
+
+console.log(calculateLoanInterest(1000, 0.05, 3)); 
+console.log(calculateLoanInterest(5000, 0.07, 5));
