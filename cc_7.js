74c14cab36d42815a946e62f338481275ccd12c6
Tyler Paiewonsky <tylerpsky@gmail.com>
2025-02-16 13:10:59 -0500

    Task 3 - Customer Loyalty Discount.

--- cc_7.js
+++ cc_7.js
@@ -19,4 +19,22 @@ const calculateHourlyWage = function(salary, hoursPerWeek) {
 console.log(calculateHourlyWage(52000,40));
 console.log(calculateHourlyWage(75000,35));
 
+//Task 3
+
+const calculateLoyaltyDiscount = (amount, years) => {
+    let discountRate = 0.05; 
+
+    if (years >= 5) {
+        discountRate = 0.15;
+    } else if (years >= 3) {
+        discountRate = 0.10;
+    }
+
+    let discountedPrice = amount * (1 - discountRate);
+    return `Discounted Price: $${discountedPrice.toFixed(2)}`;
+};
+
+console.log(calculateLoyaltyDiscount(100, 6));
+console.log(calculateLoyaltyDiscount(200, 2));
+
 
