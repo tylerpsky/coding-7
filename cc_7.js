dfe103cdef6abf6279d4fdfc8becc45ac2cccc38
Tyler Paiewonsky <tylerpsky@gmail.com>
2025-02-16 12:55:09 -0500

    Task 2 - Employee Hourly Wage Calculation.

--- cc_7.js
+++ cc_7.js
@@ -1,11 +1,22 @@
 //Task 1
 
 function calculateInvoice(subtotal, taxRate, discount) {
-    return (subtotal + subtotal * taxRate - discount).toFixed(2);
+
+    let total = (subtotal + (subtotal * taxRate)) - discount;
+    return 'Total Invoice: $${total.toFixed(2)}';
 }
 
 console.log(calculateInvoice(100, 0.08, 5)); 
 console.log(calculateInvoice(500, 0.1, 20));
 
 //Task 2
+
+const calculateHourlyWage = function(salary, hoursPerWeek) {
+    let hourlyWage = salary / (hoursPerWeek * 52);
+    return `Hourly Wage: $${hourlyWage.toFixed(2)}`;
+};
+
+console.log(calculateHourlyWage(52000,40));
+console.log(calculateHourlyWage(75000,35));
+
 
